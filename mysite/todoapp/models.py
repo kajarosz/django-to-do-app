77737008e920aa4a8cc363b2bc7dc74448ea251c
kajarosz <karolinaannajarosz@gmail.com>
2022-03-20 19:46:07 +0100

    Added ToDoList model. Updates Task model by adding todolist foreign key.

--- mysite/todoapp/models.py
+++ mysite/todoapp/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class ToDoList(models.Model):
+    todolist = models.CharField(max_length=200)
+
 class Task(models.Model):
     task = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     notes = models.TextField(default=None, null=True, blank=True)
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE, default=1)
