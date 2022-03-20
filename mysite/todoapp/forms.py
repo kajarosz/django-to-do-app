c949d69516f11a1fb1275cc5a23ee165f8c7c39b
kajarosz <karolinaannajarosz@gmail.com>
2022-03-20 19:52:15 +0100

    Added ToDoList model form.

--- mysite/todoapp/forms.py
+++ mysite/todoapp/forms.py
@@ -1,8 +1,13 @@
 from wsgiref.headers import tspecials
 from django import forms
-from .models import Task
+from .models import Task, ToDoList
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['task', 'completed', 'notes']
+
+class ToDoListForm(forms.ModelForm):
+    class Meta:
+        model = ToDoList
+        fields = ['todolist']
